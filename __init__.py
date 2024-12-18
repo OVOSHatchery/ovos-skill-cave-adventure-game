2a994144d5a566f7df7edce2e6db7744ec34c0ea
miro <jarbasai@mailfence.com>
2024-12-18 17:35:42 +0000

    frotz

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-from ovos_workshop.decorators import conversational_intent, intent_handler
 from pyfrotz.ovos import FrotzSkill
 from pyfrotz.parsers import advent_intro_parser
 
@@ -6,25 +5,9 @@ from pyfrotz.parsers import advent_intro_parser
 class ColossalCaveAdventureSkill(FrotzSkill):
     def __init__(self, *args, **kwargs):
         # game is english only, apply bidirectional translation
-        super().__init__(game_id="Advent",
+        super().__init__(*args,
+                         game_id="Advent",
                          game_lang="en-us",
                          game_data=f'{self.root_dir}/res/{self.game_id}.z5',
                          intro_parser=advent_intro_parser,
-                         *args, **kwargs)
-
-    @intent_handler("play.intent")
-    def handle_play(self, message=None):
-        self.start_game(load_save=True)
-
-    # intents
-    @conversational_intent("exit.intent")
-    def handle_exit(self, message=None):
-        self.exit_game()
-
-    @conversational_intent("restart_game.intent")
-    def handle_restart(self, message=None):
-        self.start_game(load_save=False)
-
-    @conversational_intent("save.intent")
-    def handle_save(self, message=None):
-        self.save_game()
+                         **kwargs)
