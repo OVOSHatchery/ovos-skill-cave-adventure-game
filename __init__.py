244d77f398c4396f7e740876f8cb978c939458f4
miro <jarbasai@mailfence.com>
2024-12-18 17:48:37 +0000

    frotz

--- __init__.py
+++ __init__.py
@@ -5,9 +5,14 @@ from pyfrotz.parsers import advent_intro_parser
 class ColossalCaveAdventureSkill(FrotzSkill):
     def __init__(self, *args, **kwargs):
         # game is english only, apply bidirectional translation
+        # TODO - dedicated icon, use pyfrotz icon for now
+        skill_icon = "https://raw.githubusercontent.com/TigreGotico/pyFrotz/refs/heads/dev/pyfrotz/gui/all/pyfrotz.png"
+        bg = "https://raw.githubusercontent.com/TigreGotico/pyFrotz/refs/heads/dev/pyfrotz/gui/all/bg.png"
         super().__init__(*args,
                          game_id="Advent",
                          game_lang="en-us",
                          game_data=f'{self.root_dir}/res/{self.game_id}.z5',
                          intro_parser=advent_intro_parser,
+                         skill_icon=skill_icon,
+                         game_image=bg,
                          **kwargs)
